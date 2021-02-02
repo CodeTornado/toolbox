243d2b0b9efac9677e3450470d04b47b61e9cebe
Shimmer <15600559182@163.com>
2021-02-03 02:47:26 +0800

    爬一下有道词典  例句读音丢到页面中  完善词典功能页面 一切正常 完成

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,23 @@
 package com.example.demo;
 
+import com.sun.istack.internal.NotNull;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @Controller
@@ -21,5 +35,46 @@ public class DemoApplication {
     @RequestMapping("/")
     public String random_pw() {
         return "random_pw";
+    }
+
+    @RequestMapping("word_translation")
+    public String wordTranslation() {
+        return "word_translation";
+    }
+
+    @RequestMapping("wordTranslationData")
+    @ResponseBody
+    public List wordTranslationData(@RequestParam("word") String word) {
+        if (word == null || "".equals(word)) {
+            return null;
+        }
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl = "http://dict.youdao.com/example/mdia/" + word + "/#keyfrom=dict.main.moremedia";
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+
+        Document document = Jsoup.parse(response.getBody());
+
+        Elements pElements = document.getElementById("originalSound").select("li p:eq(0)");
+
+        List list = new ArrayList<Map>();
+
+        for (Element pEle : pElements) {
+
+            Map map = new HashMap<String, String>();
+            Elements aEles = pEle.getElementsByTag("a");
+            if (aEles != null && aEles.size() > 0) {
+                Element aEle = aEles.get(0);
+                String voiceUrlStr = aEle.attr("data-rel");
+
+                map.put("voice", voiceUrlStr);
+                aEles.remove();
+            }
+            String exampleSentenceStr = pEle.text();
+
+            map.put("example_sentence", exampleSentenceStr);
+            list.add(map);
+        }
+
+        return list;
     }
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,19 @@
 package com.example.demo;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -10,4 +22,46 @@ class DemoApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void restTemplateOpenPage() {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl = "https://blog.csdn.net/cd826_dong/article/details/76652015";
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+        System.out.println(response.getBody());
+    }
+
+    @Test
+    void jsoupPageParsing() {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl = "http://dict.youdao.com/example/mdia/hello/#keyfrom=dict.main.moremedia";
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+        System.out.println("---------------------华丽的分割线A-------");
+        System.out.println(response.getBody());
+        System.out.println("---------------------华丽的分割线B-------");
+        Document document = Jsoup.parse(response.getBody());
+
+
+        Elements pElements = document.getElementById("originalSound").select("li p:eq(0)");
+
+        List list = new ArrayList<Map>();
+
+        for (Element pEle : pElements) {
+
+            Map map = new HashMap<String, String>();
+            Elements aEles = pEle.getElementsByTag("a");
+            if (aEles != null && aEles.size() > 0) {
+                Element aEle = aEles.get(0);
+                String voiceUrlStr = aEle.attr("data-rel");
+
+                map.put("voice", voiceUrlStr);
+                aEles.remove();
+            }
+            String exampleSentenceStr = pEle.text();
+
+            map.put("example_sentence", exampleSentenceStr);
+            list.add(map);
+        }
+
+        System.out.println("list.toString() = " + list.toString());
+    }
 }
